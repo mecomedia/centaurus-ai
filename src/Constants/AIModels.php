a6166e92e29378127b0c9d375416fe25af15fb64
Markus Weiss <mw@mecomedia.de>
2025-12-05 22:00:27 +0100

    Update der Models

--- src/Constants/AIModels.php
+++ src/Constants/AIModels.php
@@ -7,6 +7,7 @@ namespace Mecomedia\CentaurusAI\Constants;
  */
 class AIModels
 {
+    /* OpenAI Models */
     public const OPENAI_MODEL_4O = "chatgpt-4o-latest";
     public const OPENAI_MODEL_4O_MINI = "gpt-4o-mini";
     public const OPENAI_MODEL_41 = "gpt-4.1";
@@ -14,19 +15,32 @@ class AIModels
     public const OPENAI_MODEL_50 = "gpt-5";
     public const OPENAI_MODEL_50_MINI = "gpt-5-mini";
     public const OPENAI_MODEL_50_NANO = "gpt-5-nano";
+    public const OPENAI_MODEL_51 = "gpt-5.1";
     public const OPENAI_MODEL_O1 = "o1";
     public const OPENAI_MODEL_O1_MINI = "o1-mini-2024-09-12";
     public const OPENAI_MODEL_O3 = "o3";
     public const OPENAI_MODEL_O3_MINI = "o3-mini";
     public const OPENAI_EMBEDDING_3_LARGE = "text-embedding-3-large";
+
+    /* Claude Models */
+    public const CLAUDE_HAIKU_45 = "claude-haiku-4-5-20251001";
+    public const CLAUDE_OPUS_45 = "claude-opus-4-5-20251101";
     public const CLAUDE_OPUS_41 = "claude-opus-4-1-20250805";
     public const CLAUDE_OPUS_4 = "claude-opus-4-20250514";
+    public const CLAUDE_SONNET_45 = "claude-sonnet-4-5-20250929";
     public const CLAUDE_SONNET_4 = "claude-sonnet-4-20250514";
     public const CLAUDE_SONNET_37 = "claude-3-7-sonnet-latest";
+
+    /* Gemini Models */
+    public const GEMINI_3_PRO = 'models/gemini-3-pro-preview';
     public const GEMINI_25_PRO = 'models/gemini-2.5-pro';
     public const GEMINI_25_FLASH = 'models/gemini-2.5-flash';
     public const GEMINI_25_LITE = 'models/gemini-2.5-flash-lite';
     public const GEMINI_2_FLASH = 'models/gemini-2.0-flash';
+
+    /* Mistral Models */
     public const MISTRAL_LARGE = 'mistral-large-latest';
+
+    /* IONOS Models */
     public const IONOS_LAMA_33 = "meta-llama/Llama-3.3-70B-Instruct";
 }
